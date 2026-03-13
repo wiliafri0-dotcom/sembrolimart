62630c0ce26921d50d1df3e63e8ca0c4646bde8f
wiliafri0-dotcom <wiliafri0@gmail.com>
2026-03-13 08:51:36 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -203,7 +203,7 @@ function App() {
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
               type="text"
-              placeholder="Cari Product"
+              placeholder="Cari Product..."
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-500 focus:border-transparent outline-none"
